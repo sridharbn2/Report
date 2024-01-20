c58702ab4a2829bb0af8aba66fd47f486a56f4b5
sridharbn2 <149517014+sridharbn2@users.noreply.github.com>
2024-01-20 05:53:33 +0530

    Update table_view_type_index_alter.sql

--- v24.1/table_view_type_index_alter.sql
+++ v24.1/table_view_type_index_alter.sql
@@ -1,2 +1,15 @@
 --liquibase formatted sql
 
+--sridhar:kcIpyq7J labels:v24.01 
+
+create table test1 (
+  tid      integer, 
+  tname    varchar(10), 
+  tstatus  varchar(1),
+  tstatus2 varchar(1)
+);
+
+alter table test1 add constraint test1_pk primary key (tid);
+
+--rollback alter table test1 drop constraint test1_pk;
+--rollback drop table test1;
